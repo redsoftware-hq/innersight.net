96cfef48dc8646cd23e8d5cccd6f423ff3e18333
Ronak Baldha <5601641+ron4ex@users.noreply.github.com>
2020-02-02 21:00:54 +0530

    intial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import Logo from "../components/Logo"
+import SEO from "../components/SEO"
 
 const IndexPage = () => (
   <Layout>
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Logo />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
